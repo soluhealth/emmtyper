6eae1125b728f52b99050b8dbc7b8bc2f48e590e
Andre-Tan <andre.sutanto.91@gmail.com>
2017-05-05 16:46:26 +1000

    BlastChecker == filterBlastOutput and removed a bug with "#". executeBlast now uses subprocess instead of os.

--- emmail/utils/executeBlast.py
+++ emmail/utils/executeBlast.py
@@ -1,50 +1,98 @@
-from Bio.Blast.Applications import NcbiblastnCommandline
 import argparse
-import os
+import subprocess
+import filterBlastOutput
 
 def buildParser():
-    parser = argparse.ArgumentParser(description="Python script to run Blastn from command line.")
+    parser = argparse.ArgumentParser(description="Run Blastn from command line.")
 
-    parser.add_argument("-makeblastDB", action="store_true", dest="makeDB", help="Will make blast db on working directory if mentioned.")
-    
-    parser.add_argument("--db", required=True, help="The database to BLAST against.")
-    parser.add_argument("--query", required=True, help="Query FASTA.")
-    parser.add_argument("-dust", default="no", help="Filter query sequence with DUST, as defined in BLAST. Default no.")
-    parser.add_argument("-perc_identity", default=95, help="Minimal percent identity, as in BLAST. Default 95.")
-    parser.add_argument("-culling_limit", default=1, help="Number of hits to return in a single position, as in BLAST. Default 1.")
-    parser.add_argument("-outfmt", default="6 std slen", help="Intended output format, as in BLAST. Default \"6 std slen\".")
-    parser.add_argument("-out", default="-", help="File output, as in BLAST. Default output to terminal.")
-    
+    parser.add_argument("-makeblastDB", action="store_true", dest="makeDB",
+                        help="Make blast db on directory on mention.")
+
+    parser.add_argument("--db", required=True,
+                        help="The database to BLAST against.")
+    parser.add_argument("--query", required=True,
+                        help="Query FASTA.")
+    parser.add_argument("-dust", default="no",
+                        help="Filter query sequence with DUST. Default no.")
+    parser.add_argument("-perc_identity", default=95,
+                        help="Minimal percent identity of sequence. Default 95.")
+    parser.add_argument("-culling_limit", default=1,
+                        help="Total hits to return in a position. Default 1.")
+    parser.add_argument("-outfmt", default="6 std slen",
+                        help="Output format as in BLAST. Default \"6 std slen\".")
+    parser.add_argument("-out", default="-",
+                        help="File to stream output. Default to terminal.")
+
     return parser
 
 def makeblastDB(args):
     """
     We might need to make a blastDB if the DB is not set up.
     """
-    
+
     db_in = args.db
     db_name = db_in.split("/")[-1].split(".")[0]
-    
+
     string = "makeblastdb -in {0} -dbtype {1} -title {2} -hash_index -out {2}"
     string = string.format(args.db, "nucl", db_name)
-    
-    print(string)
+
     # os.system(string)
+    subprocess.call(string)
 
 def runBlast(args):
-    string = "blastn -db {0} -query {1} -dust {2} -perc_identity {3} -culling_limit {4} -outfmt {5} -out {6}"
-    
-    string = string.format(args.db, args.query, args.dust, args.perc_identity, 
-                           args.culling_limit, "\"{}\"".format(args.outfmt), args.out)
-    
-    print(string)
-    # os.system(string)
+    """
+    Using args from argparse object, build a unix command to run BLAST.
 
-parser = buildParser()
-args = parser.parse_args()
+    Input: args from argparse.argumentParser().
+    Output: stdout and stderr of the subprocess run.
+    """
+    string = ("blastn -db {0} -query {1} -dust {2} -perc_identity {3}"
+                " -culling_limit {4} -outfmt {5} -out {6}")
 
-if args.makeDB == True:
-    makeblastDB(args)
-    args.db = args.db.split("/")[-1].split(".")[0]
+    string = string.format(args.db, args.query, args.dust,
+                            args.perc_identity, args.culling_limit,
+                            "\"{}\"".format(args.outfmt), args.out)
 
-runBlast(args)
+    process = subprocess.Popen(args=string, shell=True,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+
+    return process.communicate()
+
+
+def stderr_logic_filter(stderr):
+    """
+    Some stderr that we need not worry about.
+    stderr within the list ignore_err will not be printed out.
+    """
+    ignore_err = [("FASTA-Reader: "
+                    "Title ends with at least 20 valid nucleotide characters."
+                    "  Was the sequence accidentally put in the title line?\n")]
+
+    if stderr not in ignore_err:
+        print stderr
+
+def main():
+    parser = buildParser()
+    args = parser.parse_args()
+
+    # If user wants to make db, make one in working directory.
+    # Assume no options change to makeblastdb.
+    if args.makeDB == True:
+        makeblastDB(args)
+        args.db = args.db.split("/")[-1].split(".")[0]
+
+    stdout, stderr = runBlast(args)
+
+    # Divide BLAST output to lines, and process whether to print stderr or not.
+    outputs = stdout.split("\n")[:-1]
+    stderr_logic_filter(stderr)
+
+    # Filter BLAST outputs and return.
+    # Consider building options to filter outputs based on user intent.
+    filtered_outputs = filterBlastOutput.filter(outputs)
+
+    return(filtered_outputs)
+
+if __name__ == "__main__":
+    main()
--- emmail/utils/filterBlastOutput.py
+++ emmail/utils/filterBlastOutput.py
@@ -1,80 +1,76 @@
-import sys
+# import sys
 
 # Main workhorse.
 
-def BlastChecker(variable):
+def filter(variable):
     # Change arguments here.
     # IMPORTANT TO KNOW: FUNCTIONS DO NOT AUTOMATICALLY RELATE TO EACH OTHER. USE ALL NEEDED.
-    
-    variableList = ['Query', 'Blast Hit', 'Identity', 'Alignment Length', 'Mismatch', 
-                       'Gap Open', 'Query Start', 'Query End', 'Hit Start', 'Hit End', 
+
+    variableList = ['Query', 'Blast Hit', 'Identity', 'Alignment Length', 'Mismatch',
+                       'Gap Open', 'Query Start', 'Query End', 'Hit Start', 'Hit End',
                        'E-Value', 'Bit Score', 'Subject Length']
-    
+
     line = ""
     for var in variableList:
         line += "{}\t".format(var)
-    print(line)
-    
-    for line in variable.readlines():
-        line_tmp = line.split()
-    
-        # If line is comment, ignore.
-        if "#" in line_tmp[0]:
-            break
-        
-        # Else, build dictionary of variables in tab.
-        else:
-        
-            Vars = {}
-        
-            for i in range(len(variableList)):
-                if line_tmp[i].isdigit():
-                    Vars[variableList[i]] = int(line_tmp[i])
-                else:
-                    Vars[variableList[i]] = line_tmp[i]
-        
+    print line
+
+    for line in variable:
+
+        line_tmp = line.split("\t")
+
+        # Build dictionary of variables in tab
+        Vars = {}
+
+        for i in range(len(variableList)):
+            if line_tmp[i].isdigit():
+                Vars[variableList[i]] = int(line_tmp[i])
+            else:
+                Vars[variableList[i]] = line_tmp[i]
+
         if mismatch_k(Vars, 4) and alignment_to_subject_length_k(Vars, 5) and gap_k(Vars, 2):
-            print(line[0:-1])
-                
-# Argument functions.                
-                
+            print line[0:-1]
+
+# Argument functions.
+
 def alignment_to_subject_length_k(dict, k = 0):
-    # Check whether alignment length within minimum k threshold to subject length.
-    
+    # Check whether alignment length is within minimum k threshold to subject length.
+
     return dict["Subject Length"] - dict["Alignment Length"] <= k
 
 # Accounting for possible mismatches.
 
 def mismatch_k(dict, k = 0):
-    # Takes into account possibility of k mismatch(es) as okay for classification.
+    # Accounts possibility of k mismatch(es) as okay for classification.
     # 1 Gap open satisfies need.
-    
+
     return dict["Mismatch"] <= k
-    
+
 def gap_k(dict, k = 0):
     # Default allows all ungapped lines.
-    
+
     return dict["Gap Open"] <= k
 
 def hit_start_end_k(dict, k = 0):
-    # Reduces need for alignment length being the same as subject length by k value.
+    # Reduces need for alignment length to be same as subject length by k value.
     # Requires other functions to work well, such as mismatch functions.
-    
+
     start = 1 + k
     end = dict["Subject Length"] - k
-    
+
     start_bool = dict["Hit Start"] <= start or dict["Hit Start"] >= end
     end_bool = dict["Hit End"] <= start or dict["Hit End"] >= end
-    
+
     return start_bool and end_bool
 
 def bit_score_346(dict, bit = 346):
     # Check whether bit score is full.
-    
-    return dict["Bit Score"] == bit
-    
-# Runs
 
+    return dict["Bit Score"] == bit
+
+# Runs on terminal using command below.
+"""
 data = sys.stdin
 
 BlastChecker(data)
+"""
