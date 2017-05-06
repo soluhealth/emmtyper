9d8ea8f15cd03f5023474f3d6b99e95a4afae1ee
Andre-Tan <andre.sutanto.91@gmail.com>
2017-05-07 01:33:06 +1000

    Updated main script to current design.

--- emmail/emmail.py
+++ emmail/emmail.py
@@ -1,3 +1,26 @@
-from runsrst2 import main as srst2
+import argparse
+import subprocess
+import utils.executeBlast as executeBlast
 
-srst2()
+def buildParser():
+    parser = argparse.ArgumentParser(description="EmMAIL - emm Automatic Isolate Labeler")
+    subparsers = parser.add_subparsers(title='Pipelines to choose from',
+                                       metavar='',
+                                       help="Explanation given")
+
+    # BLAST subparser
+    parser_blast = subparsers.add_parser("blast", help="Direct BLAST to genome reads.")
+    parser_blast = executeBlast.buildSubparser(parser_blast)
+
+    # PCR + BLAST subparser
+    parser_pcr = subparsers.add_parser("pcr", help="PCR genome, then BLAST against it.")
+
+    return parser
+
+def main():
+    parser = buildParser()
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
