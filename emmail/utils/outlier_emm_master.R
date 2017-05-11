3334387b5ff521acd7e341ffdbc632a4b1a561c9
Andre-Tan <andre.sutanto.91@gmail.com>
2017-05-11 17:12:45 +1000

    Added comments on Rscript

--- emmail/utils/outlier_emm_master.R
+++ emmail/utils/outlier_emm_master.R
@@ -1,12 +1,17 @@
+
+# Command line arguments
 args = commandArgs(trailingOnly = T)
 
-if (length(args) == 2) {
-  multiplier = as.integer(args[2])
+# For now, only catering for file input and range multiplier
+file_input = args[1]
+
+if (length(args) > 1) {
+  multiplier = as.double(args[2])
 } else {
   multiplier = 1
 }
 
-table = read.table(args[1], sep="\t")
+tab = read.table(file_input, sep="\t")
 
 nameTable = function(df) {
   # Name table, based on BLAST results
@@ -15,8 +20,9 @@ nameTable = function(df) {
   return(df)
 }
 
-table = nameTable(table)
-table = table[table$AlLength > 80,]
+# Name data frame, and filter the ones with alignment length lower that 80.
+tab = nameTable(tab)
+tab = tab[tab$AlLength > 80,]
 
 mean = mean(table$Qstart)
 median = median(table$Qstart)
@@ -24,8 +30,12 @@ sd = sd(table$Qstart)
 
 print(c("Mean: ", mean)); print(c("SD: ", sd))
 
-zVals = sapply(table$Qstart, function(x) {dnorm((x - mean)/sd)})
+# Calculate z-value of every query start.
+pVals = sapply(table$Qstart, function(x) {dnorm((x - mean)/sd)})
 
+# First: show those outside a threshold of mean and multiplier * sd
 as.vector(subset(table, Qstart <= (mean - multiplier*sd) | Qstart >= (mean + multiplier*sd))$EMM)
-as.vector(table[zVals*2 < 0.05,]$EMM)
+# Second: show the ones with signifinicance on (presumed) normal distribution
+as.vector(table[pVals < 0.025 | pVals > 0.975,]$EMM)
+# Third: show the ones outside +- 500 range from median
 as.vector(subset(table, Qstart <= (median - 500) | Qstart >= (median + 500))$EMM)
