0263f67aae28e2ea2b760e45b628c4d658e23bfc
Andre-Tan <andre.sutanto.91@gmail.com>
2017-07-22 18:04:02 +1000

    Commented out add_header for now

--- emmail/utilities/runBLAST.py
+++ emmail/utilities/runBLAST.py
@@ -4,8 +4,6 @@ def buildSubparser(parser):
     """
     Add arguments to the subparser, and return the subparser.
     """
-    # parser = argparse.ArgumentParser(description="Run Blastn from command line.")
-
     parser.add_argument("--db", required=True,
                         help="The database to BLAST against.")
     parser.add_argument("--query", required=True,
@@ -22,15 +20,18 @@ def buildSubparser(parser):
                         help="Total hits to return in a position. Default 1.")
     parser.add_argument("-outfmt", default="6 std slen",
                         help="Output format as in BLAST. Default \"6 std slen\".")
-    parser.add_argument("-out", action="store",
+    parser.add_argument("-outBLAST", default=None, action="store",
                         help="File to stream output. Default to terminal.")
 
     return parser
 
 def main(args):
-    blast = BLAST(args.db, args.query, 
-                    args.dust, args.perc_identity,
-                    args.culling_limit, 
-                    args.outfmt, args.out)
+    blast = BLAST(db = args.db, 
+                    query = args.query, 
+                    dust = args.dust, 
+                    perc_identity = args.perc_identity,
+                    culling_limit = args.culling_limit, 
+                    outfmt = args.outfmt, 
+                    out = args.outBLAST)
                     
     blast.run_blastn_pipeline()
