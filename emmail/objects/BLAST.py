712b8f1773239587f06b3312086f44d58c5c9fbd
Andre-Tan <andre.sutanto.91@gmail.com>
2017-07-22 18:05:49 +1000

    Changed output writing behavior

--- emmail/objects/BLAST.py
+++ emmail/objects/BLAST.py
@@ -1,20 +1,25 @@
-from os import path
+from os import path, environ
+import logging
 import subprocess
 
 from emmail.objects.Command import Command, FileNotInPathException
 from emmail.objects.Row import Row
 
+logging.basicConfig(level=environ.get("LOGLEVEL", "INFO"))
+logger = logging.getLogger(__name__)
+
 class BLAST(Command):
     
     def __init__(self, db, query, dust,
                 perc_identity, culling_limit,
-                outfmt, out=None):
+                outfmt, out):
                 
         Command.__init__(self, "blastn")
         
         self.version = self.get_version()
         
         self.db = self.assert_db_and_return(db)
+        
         self.query = Command.assert_filepath_and_return(query)
         
         self.dust = dust
@@ -34,8 +39,10 @@ class BLAST(Command):
                     "Query = {1}\n"
                     "DB = {2}\n"
                     "Dust filter = {3}\n"
-                    "% Identity = {4}\nCulling limit = {5}\n"
-                    "Outformat = {6}\nOutput to = {7}\n")
+                    "% Identity = {4}\n"
+                    "Culling limit = {5}\n"
+                    "Outformat = {6}\n"
+                    "Output to = {7}\n")
         
         string = string.format(self.version, self.query, self.db, self.dust, self.perc_identity, 
                                 self.culling_limit, self.outformat, self.output_stream)
@@ -52,8 +59,7 @@ class BLAST(Command):
         
         return stdout.decode("ascii")
     
-    def assert_db_and_return(self, file_path):
-    
+    def assert_db_and_return(self, file_path):  
         if not (path.isfile(file_path+".nin") and path.isfile(file_path+".nhr") and path.isfile(file_path+".nsq")):
             raise FileNotInPathException("{} is not a BLAST database!".format(file_path))
             exit(1)
@@ -61,7 +67,6 @@ class BLAST(Command):
         return file_path
         
     def build_blastn_command(self):
-        
         string = ("blastn -db {0} -query {1} -dust {2} -perc_identity {3}"
                     " -culling_limit {4} -outfmt {5}")
         
@@ -77,41 +82,23 @@ class BLAST(Command):
         
         return ok_results
     
-    def alternate_filter_blastn_rows(self, outputs):
-        
-        def buildGenerator(outputs):
-            for output in outputs:
-                yield output
-        
-        ok_results = []
-        
-        for output in buildGenerator(outputs):    
-            if Row(output).filterMe() is not None:
-                ok_results.append(Row(output))
-            
-        return ok_results
-    
-    def write_where(self, filtered_outputs):
+    def row_to_output(self, filtered_outputs):
         string = ""
-        
+       
         for output in filtered_outputs:
             string += repr(output) + "\n"
         
-        if self.output_stream == None or self.output_stream == "None":
+        if self.output_stream in [None, "None", "stdout"]:
             print(string[:-1])
         else:
             with open(self.output_stream, "w") as handle:
                 handle.write(string[:-1])
-            
-        return string[:-1]
-    
-    def run_blastn_pipeline(self):
         
-        stdout = Command.run_command(self)
-            
-        # Divide BLAST output to lines, and process whether to print stderr or not.
-        outputs = stdout.split("\n")[:-1]
+    def run_blastn_pipeline(self):
+        logger.info("Running on {}".format(self.query))
+        
+        outputs = Command.run(self).split("\n")[:-1]
         
         filtered_outputs = self.filter_blastn_rows(outputs)
         
-        return self.write_where(filtered_outputs)
+        return self.row_to_output(filtered_outputs)
