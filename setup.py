c89830c03eb75f11c9b38d0161bc67a538026059
Andre-Tan <andre.sutanto.91@gmail.com>
2017-07-27 14:26:21 +1000

    setup.py takes info from init

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
+import sys
+import os
+import logging
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+
+if sys.version_info <= (3, 0):
+    logging.fatal("Sorry, requires Python 3.x, not Python 2.x\n")
+    sys.exit(1)
+
 from setuptools import setup
+import emmail
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
-setup(name="emmail",
-        version="0.5",
-        description="EMM Automatic Isolate Labeler",
+setup(name= emmail.__name__,
+        version = emmail.__version__,
+        description = emmail.__description__,
         long_description=long_description,
-        url="https://github.com/Andre-Tan/EmMAIL",
-        author="Andre Tan",
-        author_email="andre.sutanto.91@gmail.com",
-        license="GPL-3.0",
+        url = emmail.__url__,
+        author = emmail.__author__,
+        author_email= emmail.__email__,
+        license = emmail.__license__,
         packages=["emmail"],
         zip_safe=False,
-        # install_requires=["biopython"],
         test_suite="nose.collector",
         tests_require=["nose"],
         scripts=["bin/emmail"],
