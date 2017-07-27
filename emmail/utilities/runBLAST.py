3ff0e75c899c3d65de7522ef76da82423c8fca45
Andre-Tan <andre.sutanto.91@gmail.com>
2017-07-27 14:27:01 +1000

    Updated types on argparse

--- emmail/utilities/runBLAST.py
+++ emmail/utilities/runBLAST.py
@@ -4,25 +4,24 @@ def buildSubparser(parser):
     """
     Add arguments to the subparser, and return the subparser.
     """
-    parser.add_argument("--db", required=True,
+    parser.add_argument("--db", required=True, type=str,
                         help="The database to BLAST against.")
-    parser.add_argument("--query", required=True,
+    parser.add_argument("--query", required=True, type=str,
                         help="Query FASTA.")
 
-    #parser.add_argument("-add_header", action="store_true", dest="header",
-                        #help="Add header to the output file on mention.")
-                        
-    parser.add_argument("-dust", default="no",
+    parser.add_argument("-dust", default="no", type=str,
                         help="Filter query sequence with DUST. Default no.")
-    parser.add_argument("-perc_identity", default=95,
+    parser.add_argument("-perc_identity", default=95, type=int,
                         help="Minimal percent identity of sequence. Default 95.")
-    parser.add_argument("-culling_limit", default=1,
+    parser.add_argument("-culling_limit", default=1, type=int,
                         help="Total hits to return in a position. Default 1.")
-    parser.add_argument("-outfmt", default="6 std slen",
-                        help="Output format as in BLAST. Default \"6 std slen\".")
-    parser.add_argument("-outBLAST", default=None, action="store",
+    parser.add_argument("-outBLAST", default="None", 
+                        action="store", type=str,
                         help="File to stream output. Default to terminal.")
 
+    parser.add_argument("-add_header", default=False, action="store_true", 
+                        help="Add header to the output file on mention.")
+                        
     return parser
 
 def main(args):
@@ -31,7 +30,8 @@ def main(args):
                     dust = args.dust, 
                     perc_identity = args.perc_identity,
                     culling_limit = args.culling_limit, 
-                    outfmt = args.outfmt, 
-                    out = args.outBLAST)
+                    
+                    out = args.outBLAST,
+                    header = args.add_header)
                     
     blast.run_blastn_pipeline()
--- emmail/utilities/runIsPCR.py
+++ emmail/utilities/runIsPCR.py
@@ -1,3 +1,6 @@
+from tempfile import NamedTemporaryFile
+from os import remove
+
 from emmail.objects.IsPCR import IsPCR
 from emmail.objects.BLAST import BLAST
 
@@ -5,64 +8,72 @@ def buildSubparser(parser):
     """
     Add arguments to the subparser, and return the subparser.
     """
-    parser.add_argument("--genome", required=True,
+    parser.add_argument("--genome", required=True, type=str,
                         help="The genome to PCR against.")
-    parser.add_argument("--primer", required=True,
+    parser.add_argument("--primer", required=True, type=str,
                         help="PCR primer. Text file with 3 columns: Name, Forward Primer, Reverse Primer.")
-    parser.add_argument("--db", required=True,
+    parser.add_argument("--db", required=True, type=str,
                         help="The database to BLAST PCR product against.")
 
     # isPcr options
-    
-    parser.add_argument("-outPCR", default="pcr.tmp",
-                        help="Output filename, to use as BLAST query. Default to pcr.tmp.")
-    parser.add_argument("-minPerfect", default=15,
+                      
+    parser.add_argument("-minPerfect", default=15, type=int,
                         help="Minimum size of perfect match at 3' primer end. Default is 15.")
-    parser.add_argument("-minGood", default=15,
-                        help=("Minimum size where there must be 2 matches for each mismatch."
+    parser.add_argument("-minGood", default=15, type=int,
+                        help=("Minimum size where there must be 2 matches for each mismatch. "
                               "Default is 15; there must be 10 match in 15bp primer size."))
-    parser.add_argument("-minSize", default=0,
+    parser.add_argument("-minSize", default=0, type=int,
                         help="Minimum size of PCR product. Default is 0.")
-    parser.add_argument("-maxSize", default=4000,
+    parser.add_argument("-maxSize", default=4000, type=int,
                         help="Maximum size of PCR product. Default is 4000.")
-
+    
+    parser.add_argument("-outPCR", default="pcr.tmp", type=str, 
+                        help="Output filename, to use as BLAST query. Default to pcr.tmp.")  
+    
+    parser.add_argument("-savePCR", default=False, action="store_true",
+                        help="Temporary isPcr output \"pcr.tmp\" will not be removed on mention.")
+    
     # BLAST options
 
-    parser.add_argument("-dust", default="no",
+    parser.add_argument("-dust", default="no", type=str,
                         help="Filter query sequence with DUST. Default no.")
-    parser.add_argument("-perc_identity", default=95,
+    parser.add_argument("-perc_identity", default=95, type=int,
                         help="Minimal percent identity of sequence. Default 95.")
-    parser.add_argument("-culling_limit", default=1,
+    parser.add_argument("-culling_limit", default=1, type=int,
                         help="Total hits to return in a position. Default 1.")
-    parser.add_argument("-outfmt", default="6 std slen",
-                        help="Output format as in BLAST. Default \"6 std slen\".")
-    parser.add_argument("-outBLAST", default=None, action="store",
+    parser.add_argument("-outBLAST", default="None", 
+                        action="store", type=str,
                         help="File to stream BLAST output. Default to terminal.")
 
-    #parser.add_argument("-add_header", action="store_true", dest="header",
-                        #help="Add header to the output file on mention.")
+    parser.add_argument("-add_header", action="store_true", default=False,
+                        help="Add header to the output file on mention.")
 
     return parser
     
 def main(args):
+    temp_name = "pcr.tmp"
+    
     pcr = IsPCR(assembly_filename = args.genome,
                 primer_filename = args.primer,
                 min_perfect = args.minPerfect,
                 min_good = args.minGood,
                 min_product_length = args.minSize,
                 max_product_length = args.maxSize,
-                output_stream = args.outPCR)
+                output_stream = "stdout")
     
-    pcr.run_isPCR()
-    
-    args.query = args.outPCR
+    # Run isPcr, take output and use it as input for Blast.
+    with open(temp_name, "w") as temp:
+        temp.write(pcr.run_isPCR())
     
     blast = BLAST(db = args.db, 
-                    query = args.query, 
+                    query = temp_name, 
                     dust = args.dust, 
                     perc_identity = args.perc_identity,
                     culling_limit = args.culling_limit, 
-                    outfmt = args.outfmt, 
-                    out = args.outBLAST)
+                    out = args.outBLAST,
+                    header = args.add_header)
                     
     blast.run_blastn_pipeline()
+    
+    if args.savePCR == False:
+        remove(temp_name)
