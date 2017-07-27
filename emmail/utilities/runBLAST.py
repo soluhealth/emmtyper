3ef41c0d1d3d1b08b819fc609ebf41329e539fd3
Andre-Tan <andre.sutanto.91@gmail.com>
2017-07-27 16:02:17 +1000

    Added necessary filter arguments in argparse

--- emmail/utilities/runBLAST.py
+++ emmail/utilities/runBLAST.py
@@ -4,24 +4,35 @@ def buildSubparser(parser):
     """
     Add arguments to the subparser, and return the subparser.
     """
-    parser.add_argument("--db", required=True, type=str,
-                        help="The database to BLAST against.")
     parser.add_argument("--query", required=True, type=str,
                         help="Query FASTA.")
-
+    parser.add_argument("--db", required=True, type=str,
+                        help="The database to BLAST against.")
+                        
+    # BLAST Options
+    
     parser.add_argument("-dust", default="no", type=str,
                         help="Filter query sequence with DUST. Default no.")
     parser.add_argument("-perc_identity", default=95, type=int,
-                        help="Minimal percent identity of sequence. Default 95.")
+                        help="Minimal percent identity of sequence. Default is 95.")
     parser.add_argument("-culling_limit", default=1, type=int,
-                        help="Total hits to return in a position. Default 1.")
+                        help="Total hits to return in a position. Default is 1.")
     parser.add_argument("-outBLAST", default="None", 
                         action="store", type=str,
                         help="File to stream output. Default to terminal.")
 
     parser.add_argument("-add_header", default=False, action="store_true", 
                         help="Add header to the output file on mention.")
-                        
+    
+    # Row Options
+    
+    parser.add_argument("-mismatch", default=4, type=int,
+                        help="Threshold for number of mismatch to allow in BLAST hit. Default is 4.")
+    parser.add_argument("-align_diff", default=5, type=int,
+                        help="Threshold for difference between alignment length and subject length in BLAST hit. Default is 5.")                        
+    parser.add_argument("-gap", default=2, type=int,
+                        help="Threshold gap to allow in BLAST hit. Default is 2.")
+    
     return parser
 
 def main(args):
@@ -32,6 +43,10 @@ def main(args):
                     culling_limit = args.culling_limit, 
                     
                     out = args.outBLAST,
-                    header = args.add_header)
+                    header = args.add_header,
+                    
+                    mismatch = args.mismatch,
+                    align_diff = args.align_diff,
+                    gap = args.gap)
                     
     blast.run_blastn_pipeline()
--- emmail/utilities/runIsPCR.py
+++ emmail/utilities/runIsPCR.py
@@ -1,17 +1,21 @@
-from tempfile import NamedTemporaryFile
-from os import remove
+from os import remove, environ
+import logging
 
 from emmail.objects.IsPCR import IsPCR
 from emmail.objects.BLAST import BLAST
+
+logging.basicConfig(level=environ.get("LOGLEVEL", "INFO"))
+logger = logging.getLogger(__name__)
 
 def buildSubparser(parser):
     """
     Add arguments to the subparser, and return the subparser.
     """
-    parser.add_argument("--genome", required=True, type=str,
-                        help="The genome to PCR against.")
     parser.add_argument("--primer", required=True, type=str,
                         help="PCR primer. Text file with 3 columns: Name, Forward Primer, Reverse Primer.")
+    
+    parser.add_argument("--query", required=True, type=str,
+                        help="The genome to PCR against.")
     parser.add_argument("--db", required=True, type=str,
                         help="The database to BLAST PCR product against.")
 
@@ -38,20 +42,28 @@ def buildSubparser(parser):
     parser.add_argument("-dust", default="no", type=str,
                         help="Filter query sequence with DUST. Default no.")
     parser.add_argument("-perc_identity", default=95, type=int,
-                        help="Minimal percent identity of sequence. Default 95.")
+                        help="Minimal percent identity of sequence. Default is 95.")
     parser.add_argument("-culling_limit", default=1, type=int,
-                        help="Total hits to return in a position. Default 1.")
+                        help="Total hits to return in a position. Default is 1.")
     parser.add_argument("-outBLAST", default="None", 
                         action="store", type=str,
                         help="File to stream BLAST output. Default to terminal.")
 
     parser.add_argument("-add_header", action="store_true", default=False,
                         help="Add header to the output file on mention.")
-
+    
+    # Row options
+    
+    parser.add_argument("-mismatch", default=4, type=int,
+                        help="Threshold for number of mismatch to allow in BLAST hit. Default is 4.")
+    parser.add_argument("-align_diff", default=5, type=int,
+                        help="Threshold for difference between alignment length and subject length in BLAST hit. Default is 5.")                        
+    parser.add_argument("-gap", default=2, type=int,
+                        help="Threshold gap to allow in BLAST hit. Default is 2.")
+                        
     return parser
     
 def main(args):
-    temp_name = "pcr.tmp"
     
     pcr = IsPCR(assembly_filename = args.genome,
                 primer_filename = args.primer,
@@ -62,18 +74,27 @@ def main(args):
                 output_stream = "stdout")
     
     # Run isPcr, take output and use it as input for Blast.
-    with open(temp_name, "w") as temp:
+    with open(args.outPCR, "w") as temp:
         temp.write(pcr.run_isPCR())
     
     blast = BLAST(db = args.db, 
-                    query = temp_name, 
+                    query = args.outPCR, 
                     dust = args.dust, 
                     perc_identity = args.perc_identity,
                     culling_limit = args.culling_limit, 
                     out = args.outBLAST,
-                    header = args.add_header)
+                    header = args.add_header,
+                    
+                    mismatch = args.mismatch,
+                    align_diff = args.align_diff,
+                    gap = args.gap)
                     
     blast.run_blastn_pipeline()
     
+    # logger.info("PCR and BLAST pipeline finished")
+    
     if args.savePCR == False:
-        remove(temp_name)
+        remove(args.outPCR)
+        logger.info("{} is removed from directory".format(args.outPCR))
+    else:
+        logger.info("{} is kept on directory".format(args.outPCR))
