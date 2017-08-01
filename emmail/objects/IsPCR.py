6942352089d10bcb17ee1e6f12e669a5b37f6197
Andre-Tan <andre.sutanto.91@gmail.com>
2017-08-01 12:13:54 +1000

    Deleted minSize for now

--- emmail/objects/IsPCR.py
+++ emmail/objects/IsPCR.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 class IsPCR(Command):
 
     def __init__(self, assembly_filename, primer_filename, min_perfect, min_good,
-                min_product_length, max_product_length, output_stream):
+                max_product_length, output_stream):
                 
         Command.__init__(self, "isPcr")
         
@@ -20,7 +20,6 @@ class IsPCR(Command):
         self.min_perfect = min_perfect
         self.min_good = min_good
         
-        self.min_product_length = min_product_length
         self.max_product_length = max_product_length
         self.output_stream = output_stream
         
@@ -33,12 +32,10 @@ class IsPCR(Command):
             
         string = ("isPcr {db} {query} {output} "
                     "-minPerfect={} -minGood={} "
-                    # "-minSize={} " Weirdly does not work.
                     "-maxSize={}")
         
         command = string.format(self.min_perfect,
                                 self.min_good,
-                                # self.min_product_length,
                                 self.max_product_length,
                                 db=self.assembly_filename, 
                                 query=self.primer_filename, 
@@ -50,5 +47,8 @@ class IsPCR(Command):
         logger.info("Running on {}".format(self.assembly_filename))
         
         output = Command.run(self)
+        
+        if not output:
+            logger.info("There is no output for {}".format(self.assembly_filename))
         
         return output[:-1]
