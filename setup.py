254d769d1fa1a764582bbb83e52cee6e33dccf57
Anders Goncalves da Silva <andersgs@gmail.com>
2019-08-08 15:10:00 -0700

    Add emmtyper-db entry point

--- setup.py
+++ setup.py
@@ -15,35 +15,37 @@ import emmtyper
 with open("README.md", "r") as f:
     long_description = f.read()
 
-setup(name= emmtyper.__name__,
-        version = emmtyper.__version__,
-        description = emmtyper.__description__,
-        long_description=long_description,
-        long_description_content_type='text/markdown',
-        url = emmtyper.__url__,
-        author = emmtyper.__author__,
-        author_email= emmtyper.__email__,
-        maintainer = emmtyper.__maintainer__,
-        maintainer_email = emmtyper.__maintainer_email__,
-        license = emmtyper.__license__,
-        python_requires=">=3.6, <4",
-        packages=find_packages(exclude=["contrib", "docs", "tests"]),
-        zip_safe=False,
-        install_requires = [
-            "scipy>=1.1.0",
-            "numpy>=1.15.0"],
-        test_suite="nose.collector",
-        tests_require=["nose"],
-        entry_points= {
-            "console_scripts":["emmtyper=emmtyper.bin.run_emmail:main"]
-        },
-        classifiers=[
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-            "Natural Language :: English",
-            "Programming Language :: Python :: 3 :: Only",
-            "Topic :: Scientific/Engineering :: Bio-Informatics"
-        ],
-        package_data={'emmtyper': ['objects/*.csv']}
-    )
+setup(
+    name=emmtyper.__name__,
+    version=emmtyper.__version__,
+    description=emmtyper.__description__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=emmtyper.__url__,
+    author=emmtyper.__author__,
+    author_email=emmtyper.__email__,
+    maintainer=emmtyper.__maintainer__,
+    maintainer_email=emmtyper.__maintainer_email__,
+    license=emmtyper.__license__,
+    python_requires=">=3.6, <4",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    zip_safe=False,
+    install_requires=["scipy>=1.1.0", "numpy>=1.15.0"],
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    entry_points={
+        "console_scripts": [
+            "emmtyper=emmtyper.bin.run_emmail:main",
+            "emmtyper-db:emmtyper.utilities.make_db:emmtyper_db",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    package_data={"emmtyper": ["objects/*.csv"]},
+)
