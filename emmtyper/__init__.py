068a10eb96b28332fd8c2c92ef8f9bb7d944d53d
Anders Goncalves da Silva <andersgs@gmail.com>
2019-08-15 11:46:40 -0700

    Add MDU Bioinformatics as maintainer

--- emmtyper/__init__.py
+++ emmtyper/__init__.py
@@ -1,3 +1,6 @@
+"""
+emmtyper: an in silico EMM typer for Streptococcus pyogenes
+"""
 __name__ = "emmtyper"
 __version__ = "2.0"
 __author__ = "Andre Tan"
@@ -5,5 +8,13 @@ __email__ = "andre.sutanto.91@gmail.com"
 __url__ = "https://github.com/MDUPHL/emmtyper"
 __license__ = "GPL-3.0"
 __description__ = "Streptococcus pyogenes in silico EMM typer"
-__maintainer__ = "Anders Gonçalves da Silva"
-__maintainer_email__ = "andersgs@gmail.com"
+__maintainer__ = "Bioinformatics @ MDUPHL"
+__maintainer_email__ = "anders.goncalves@unimelb.edu.au"
+
+__epilog__ = """\
+Developed by {} ({})
+Maintained by {} ({})
+Distributed under {} license
+© 2019 {}""".format(
+    __author__, __email__, __maintainer__, __maintainer_email__, __license__, __author__
+)
