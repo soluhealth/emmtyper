e487b931a8836484533241dbcc50294ccc73c9e5
Anders Goncalves da Silva <andersgs@gmail.com>
2019-09-03 11:04:01 -0700

    Add docstring

--- emmtyper/utilities/find.py
+++ emmtyper/utilities/find.py
@@ -1,7 +1,14 @@
+'''
+Define a wrapper function to find files in the package folder
+'''
 import os
 
 
 def find(filename, path):
+    '''
+    Given a filename and a path, walk all files and subfolders of path trying
+    to find an exact match to filename.
+    '''
     for root, dirs, files in os.walk(os.path.dirname(path), topdown=True):
         for name in files:
             if name == filename:
